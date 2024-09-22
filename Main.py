2c03188ed77da3ed01c985af47f0f51efc188e6c
Gandalf <quentin.pladeau@gmail.com>
2024-09-22 19:28:04 +0200

    [+] Panel Jeu base Terminé

--- Main.py
+++ Main.py
@@ -16,8 +16,31 @@ fenetre.config(bg = bgHomePage)
 fenetre.geometry("900x500")
 fenetre.title("Fractales | © Cyanne Théo Loan Quentin")
 
+# Panel des modifications
 framePanelModif = Frame(fenetre, bg = bgFramePanelModif, width=widthFrameModifPanel)
 framePanelModif.pack(side=LEFT, expand=False, fill='y', padx=10, pady=10)
+
+
+# Box du canvas et btn img + pause
+frameBoxCanvas = Frame(fenetre, bg=bgFramePanelModif)
+frameBoxCanvas.pack(expand=True, fill=BOTH)
+
+# Canvas > matplotlib canvas # A faire
+frameCanvas = Frame(frameBoxCanvas, bg="black")
+frameCanvas.pack(expand=True, fill=BOTH,side=TOP,  padx=10, pady=10)
+
+# Box boutton pause et generation d'image
+frameBoxButton = Frame(frameBoxCanvas, bg="blue")
+frameBoxButton.pack(side=BOTTOM, fill='x')
+
+# Boutton pause (gauche)
+buttonPause = Button(frameBoxButton, bg='white', width=15)
+buttonPause.pack(side=LEFT, pady=10, padx=10)
+
+# Boutton Générer une image (droite)
+buttonMakePlotToImg = Button(frameBoxButton, bg="white", width=15)
+buttonMakePlotToImg.pack(side=RIGHT, pady=10, padx=10)
+
 
 
 
