71fca523fbaa360a27ca5a2916f951285709d7a4
kaihto <tohto3006@gmail.com>
2024-09-27 11:44:38 +0200

    Update Fractales avec class + recursivité qui fonctionne !!! (non fini)

--- Fractales/ModuleFractales.py
+++ Fractales/ModuleFractales.py
@@ -6,85 +6,114 @@ class FractaleKoch:
         self.profondeur = profondeur
         self.longueur = longueur
 
-    def dessiner_koch(self, profondeur, longueur):
-        """Méthode récursive pour dessiner le flocon de Koch de profondeur profondeur et longueur longueur."""
-        if profondeur == 0:
-            forward(longueur)
+    def dessiner_koch(self, n, l):
+        """Méthode récursive pour dessiner le flocon de Koch de profondeur n et longueur l."""
+        if n == 0:
+            forward(l)
         else:
-            self.dessiner_koch(profondeur-1, longueur/3)
+            self.dessiner_koch(n-1, l/3)
             left(60)
-            self.dessiner_koch(profondeur-1, longueur/3)
+            self.dessiner_koch(n-1, l/3)
             left(-120)
-            self.dessiner_koch(profondeur-1, longueur/3)
+            self.dessiner_koch(n-1, l/3)
             left(60)
-            self.dessiner_koch(profondeur-1, longueur/3)
+            self.dessiner_koch(n-1, l/3)
     
     def dessiner(self):
         """Méthode pour dessiner le flocon de Koch en utilisant la profondeur et la longueur initiales."""
         self.dessiner_koch(self.profondeur, self.longueur)
 
-class Vicsek :
-    def __init__(self, profondeur, longueur) :
-        self.profondeur = profondeur
-        self.longueur = longueur
 
-    def carre(self, longueur):
-        speed(0)
-        color("black", "black")
+# Utilisation de la classe FractaleKoch
+if __name__ == "__main__":
+    fractale = FractaleKoch(3, 200)  # Créer une instance de FractaleKoch avec profondeur 3 et longueur 200
+    fractale.dessiner()  # Dessiner la fractale
+    mainloop()
+
+class FractaleVicsek:
+    def __init__(self, nombre, longueur):
+        """Initialisation de la fractale de Vicsek"""
+        self.nombre = nombre
+        self.longueur = longueur
+        
+    def carre(self, l):
         pendown()
         for i in range(4):
-            color("black", "black")
-            begin_fill()
-            forward(longueur)
+            forward(l)
             left(90)
-            end_fill()
-        
-    def Vicsek(self, profondeur, longueur):
+
+    def dessiner_Vicsek(self, n, l):
         x = xcor()
         y = ycor()
-        if profondeur == 0:
-            self.carre(longueur)
+        if n == 0:
+            self.carre(l)
             penup()
-            goto(x+longueur*2,y)
-            self.carre(longueur)
+            goto(x+l*2,y)
+            self.carre(l)
             penup()
-            goto(x+longueur,y+longueur)
-            self.carre(longueur)
+            goto(x+l,y+l)
+            self.carre(l)
             penup()
-            goto(x,y+longueur*2)
-            self.carre(longueur)
+            goto(x,y+l*2)
+            self.carre(l)
             penup()
-            goto(x+longueur*2,y+longueur*2)
-            self.carre(longueur)
+            goto(x+l*2,y+l*2)
+            self.carre(l)
             penup()
-        elif profondeur > 0:
-            Vicsek(profondeur-1,longueur/3)
+        elif n > 0:
+            self.dessiner_Vicsek(n-1,l/3)
             penup()
-            goto(x+longueur*2,y)
-            Vicsek(profondeur-1,longueur/3)
+            goto(x+l*2,y)
+            self.dessiner_Vicsek(n-1,l/3)
             penup()
-            goto(x+longueur,y+longueur)
-            Vicsek(profondeur-1,longueur/3)
+            goto(x+l,y+l)
+            self.dessiner_Vicsek(n-1,l/3)
             penup()
-            goto(x,y+longueur*2)
-            Vicsek(profondeur-1,longueur/3)
+            goto(x,y+l*2)
+            self.dessiner_Vicsek(n-1,l/3)
             penup()
-            goto(x+longueur*2,y+longueur*2)
-            Vicsek(profondeur-1,longueur/3)
+            goto(x+l*2,y+l*2)
+            self.dessiner_Vicsek(n-1,l/3)
             penup()
+    def dessiner(self):
+        """Méthode pour dessiner la fractale"""
+        self.dessiner_Vicsek(self.nombre, self.longueur)
+if __name__ == "__main__":
+    fractale = FractaleVicsek(3, 200)  # Créer une instance de FractaleKoch avec profondeur 3 et longueur 200
+    fractale.dessiner()  # Dessiner la fractale
+    mainloop()
 
-class Sierpinsky:
-    def __init__(self, profondeur, longueur) :
-        self.profondeur = profondeur
+class Fractale_Fibonacci:
+    def __init__(self, nombre, longueur):
+        """Initialisation de la fractale du mot de fibonacci"""
+        self.nombre = nombre
         self.longueur = longueur
-
-    def Sierpinsky(profondeur, longueur):
-        """trace un triangle de sierpinsky
-        apres profondeur itérations, à partir d'un
-        triangle equilatéral de côté longueur
-        """
-        if profondeur > 0 :
-            for i in range(3):
-                Sierpinsky(profondeur-1, longueur/2)
-                forward(longueur)
-                left(120)
+    def liste(self,n):
+        if n == 1:
+            return "B"
+        elif n == 2:
+            return self.liste(n-1) + "A"
+        elif n > 2:
+            return self.liste(n-1) + self.liste(n-2)
+    def dessiner_Fibonacci(self, nombre, l):
+        pendown()
+        mot = self.liste(nombre)
+        mot = list(mot)
+        for i in range(len(mot)):
+            if mot[i] == "B":
+                forward(l)
+            elif mot[i] == "A":
+                if (i+1)%2 == 0:
+                    right(90)
+                elif (i+1)%2 != 0:
+                    left(90)
+                forward(l)
+    def dessiner(self):
+        self.dessiner_Fibonacci(self.nombre, self.longueur)
+if __name__ == "__main__":
+    fractale = Fractale_Fibonacci(30, 2)  # CrÃ©er une instance de FractaleKoch avec profondeur 3 et longueur 200
+    penup()
+    goto(-100,0)
+    pendown()
+    fractale.dessiner()  # Dessiner la fractale
+    mainloop()
